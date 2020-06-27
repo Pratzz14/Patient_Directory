638c7b1c7552ed48bf270dc05c7a797eb0d1fce1
Pratzz14 <pratik.mahankal14@gmail.com>
2020-06-27 14:37:51 +0530

    Added code to return Patients/Pratik data

--- patient/src/main/java/com/patient/patient/PatientController.java
+++ patient/src/main/java/com/patient/patient/PatientController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,17 +24,24 @@ public class PatientController {
 		return patientService.getAllPatients();
 	}
 	
+	@RequestMapping("/Patients/{name}")
+	public Patient getPatient(@PathVariable String name) {
+		return patientService.getPatient(name);
+	}
+	
+	
+	
 	   @GetMapping("/Patients-html")
 	    public ModelAndView htmlView(Model model)
 	  {    
 		  List<Patient> patient= Arrays.asList(
 					
 					new Patient("Pratik",19,"26 June 2020",123,"Fever"),
+					new Patient("Medha",19,"26 June 2020",123,"Fever"),
+					new Patient("Janice",19,"26 June 2020",123,"Fever"),
 					new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-					new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-					new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-					new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-					new Patient("Pratik",19,"26 June 2020",123,"Feverrrr")
+					new Patient("Medha",19,"26 June 2020",123,"Fever"),
+					new Patient("Janice",19,"26 June 2020",123,"Feverrrr")
 					
 					);
 
--- patient/src/main/java/com/patient/patient/PatientService.java
+++ patient/src/main/java/com/patient/patient/PatientService.java
@@ -10,15 +10,19 @@ public class PatientService {
 	List<Patient> patient = Arrays.asList(
 			
 			new Patient("Pratik",19,"26 June 2020",123,"Fever"),
+			new Patient("Medha",19,"26 June 2020",123,"Fever"),
+			new Patient("Janice",19,"26 June 2020",123,"Fever"),
 			new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-			new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-			new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-			new Patient("Pratik",19,"26 June 2020",123,"Fever"),
-			new Patient("Pratik",19,"26 June 2020",123,"Fever")
+			new Patient("Medha",19,"26 June 2020",123,"Fever"),
+			new Patient("Janice",19,"26 June 2020",123,"Feverrrr")
 			
 			);
 	
 	public List<Patient> getAllPatients(){
 		return patient;
+	}
+	
+	public Patient getPatient(String name) {
+		return patient.stream().filter(t -> t.getName().equals(name)).findFirst().get();
 	}
 }
